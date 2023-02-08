55b9dc1062a010003315ef576474473fcfebf6ae
Tunglx <63648038+kikuchanj1806@users.noreply.github.com>
2023-02-08 16:49:22 +0700

    done

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,7 +7,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-    "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+    "https://raw.githubusercontent.com/kikuchanj1806/portfolio/main/src/Assets/CV_Fe_Le_Xuan_Tung.pdf";
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200);
@@ -27,8 +27,9 @@ function ResumeNew() {
                     </Button>
                 </Row>
                 <Row className="resume">
-                    <Document file={resumeLink} className="d-flex justify-content-center">
+                    <Document file={resumeLink} className="d-flex justify-content-center resume_flex">
                         <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                        <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
                     </Document>
                 </Row>
             </Container>
